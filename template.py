9da29cfa6e52e907b57ebdbceab01778ef737a0d
unknown <vishalajairosen.works@gmail.com>
2024-12-24 10:36:59 +0530

    Added folder structure

--- template.py
+++ template.py
@@ -4,33 +4,28 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "mlProject"
-
+project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
+    "dvc.yaml",
     "params.yaml",
-    "schema.yaml",
-    "main.py",
-    "app.py",
-    "Dockerfile",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html",
-    "test.py"]
+    "templates/index.html"
 
+
+]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -45,6 +40,7 @@ for filepath in list_of_files:
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
+
+
     else:
         logging.info(f"{filename} is already exists")
-
